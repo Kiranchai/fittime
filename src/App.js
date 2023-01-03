d458cc930c4536a83e9d83992fa512653c1f7405
Norbert <sadawa.norbert@gmail.com>
2023-01-03 21:37:14 +0100

    fixed routing

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from "./components/Home";
 import PrivateRoute from "./utils/PrivateRoute";
 import Navbar from "./components/Navbar/Navbar";
 import Account from "./components/Account/Account";
+import NotFound from "./components/NotFound/NotFound";
 import SingleExercise from "./components/SingleExercise/SingleExercise";
 import Exercises from "./components/Exercises/Exercises";
 
@@ -15,7 +16,8 @@ function App() {
       <Route path="/register" element={<Registration />} />
       <Route element={<PrivateRoute />}>
         <Route element={<Navbar />}>
-          <Route path="/" element={<Home />} />
+          <Route path="*" element={<NotFound />} />
+          {/* <Route path="/" element={<Home />} /> */}
           <Route path="/exercises" element={<Exercises />} />
           <Route path="/account" element={<Account />} />
           <Route path="/exercise/:id" element={<SingleExercise />} />
